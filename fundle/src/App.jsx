850e2cff0b27eb687492420af2de6dd0084735b4
Brian <Brianleehehe72@gmail.com>
2024-10-05 20:14:53 +0800

    Added campaign details to show up in the frontend (as well as pulling data from smart contract)

--- fundle/src/App.jsx
+++ fundle/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Home, CreateCampaign } from './pages';
+import { Home, CampaignDetails, CreateCampaign} from './pages';
 import { Navbar } from './components';
 
 const App = () => {
