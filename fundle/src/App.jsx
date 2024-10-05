2166bf6ec24dd0f62dc6112549f88268c377593f
Brian <Brianleehehe72@gmail.com>
2024-10-05 18:09:20 +0800

    Added Home Page

--- fundle/src/App.jsx
+++ fundle/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Home } from './pages';
 import { Navbar } from './components';
 
 const App = () => {
@@ -10,6 +11,10 @@ const App = () => {
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
         <Navbar />
         <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/profile' element={<Profile />}/>
+          <Route path='/create-campaign' element={<CreateCampaign />}/>
+          <Route path='/campaign-details/:id' element={<CampaignDetails />}/>
         </Routes>
       </div>
     </div>
